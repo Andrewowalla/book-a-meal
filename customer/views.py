2627badae794597d2d6d2a3fd70f3a1dc74f6291
Abdirahman <dubowhamaan@gmail.com>
2022-04-04 11:40:11 +0300

    updating views.py

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,7 @@
 import json
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import View
+from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
 
@@ -35,6 +36,11 @@ class Order(View):
         return render(request, 'customer/order.html', context)
 
     def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+
         order_items = {
             'items': []
         }
@@ -58,57 +64,38 @@ class Order(View):
             price += item['price']
             item_ids.append(item['id'])
 
-        order = OrderModel.objects.create(price=price)
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+        )
         order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
 
         context = {
             'items': order_items['items'],
             'price': price
         }
 
+        return redirect('order-confirmation', pk=order.pk)
 
-        order_items['items'].append(item_data)
-
-        price = 0
-        item_ids =[]
-
-    for item in order_items['items']:
-        price += item ['price']
-        item_ids.append(item['id'])
-
-    order = OrderModel.objects.create(
-        price=price,
-        name=name,
-        email=email,
-        street=street,
-        city=city,
-        
-        )
-    order.items.add(*item_ids)
-
-    # confirmation email to the user
-
-    body =('Thank you for your order! Your order is taken! \n'
-    f'Your total: {price}\n'
-    'Thank you for your order!')
-
-    send_mail(
-        'Thank You For your Order!',
-        body,
-        'example@example.com',
-        [email],
-        fail_silently=False
-    )
-
-    context = {
-        'items': order_items['items'],
-        'price':price
-    }
-
-    return redirect('order-confirmation', pk=order.pk)
 
 class OrderConfirmation(View):
-    def get(self,request,pk,*args,**kwargs):
+    def get(self, request, pk, *args, **kwargs):
         order = OrderModel.objects.get(pk=pk)
 
         context = {
@@ -117,7 +104,7 @@ class OrderConfirmation(View):
             'price': order.price,
         }
 
-        return render(request, 'customer/order_confirmation.html',context)
+        return render(request, 'customer/order_confirmation.html', context)
 
     def post(self, request, pk, *args, **kwargs):
         data = json.loads(request.body)
@@ -129,9 +116,7 @@ class OrderConfirmation(View):
 
         return redirect('payment-confirmation')
 
+
 class OrderPayConfirmation(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/order_pay_confirmation.html')
-
-        return render(request, 'customer/order_confirmation.html', context)
-
