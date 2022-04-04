a98483afdbc218132945e9757d36e4ae1c449cfc
annngure <annwanjikungure@gmail.com>
2022-04-04 09:58:30 +0300

    update views to view

--- customer/views.py
+++ customer/views.py
@@ -1,92 +1,68 @@
 from django.shortcuts import render
 from django.views import View
-from django.core.mail import send_mail
-from .models import *
+from .models import MenuItem, Category, OrderModel
 
 
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/index.html')
 
+
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
 
-class Order(View):
-    def get(self,request, *args, **kwargs):
-        
-        #get each item from each category
-        appetizers = MenuItem.objects.filter(category__name__contains = 'Appetizer')
-        entres = MenuItem.objects.filter(category__name__contains = 'Entre')
-        desserts = MenuItem.objects.filter(category__name__contains = 'Dessert')
-        drinks = MenuItem.objects.filter(category__name__contains = 'Drink')
 
-        #pass into context
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(
+            category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        # pass into context
         context = {
             'appetizers': appetizers,
-            'entres':entres,
-            'desserts':desserts,
-            'drinks':drinks,
-        }
-        #render the template
-        return render(request,'customer/order.html',context)
-
-def post (self,request, *args, **kwargs):
-    name = request.POST.get('name')
-    email = request.POST.get('email')
-    street = request.POST.get('street')
-    city = request.POST.get('city')
-
-    order_items = {
-        'items':[]
-    }
-
-    items = request.POST.getlist('items[]')
-
-    for item in items:
-        menu_item = MenuItem.objects.get(pk__contains=int(item))
-        item_data ={
-            'id':menu_item.pk,
-            'name':menu_item.name,
-            'price':menu_item.price
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
         }
 
-        order_items['items'].append(item_data)
+        # render the template
+        return render(request, 'customer/order.html', context)
 
-        price = 0
-        item_ids =[]
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
 
-    for item in order_items['items']:
-        price += item ['price']
-        item_ids.append(item['id'])
+        items = request.POST.getlist('items[]')
 
-    order = OrderModel.objects.create(
-        price=price,
-        name=name,
-        email=email,
-        street=street,
-        city=city,
-        
-        )
-    order.items.add(*item_ids)
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
 
-    # confirmation email to the user
+            order_items['items'].append(item_data)
 
-    body =('Thank you for your order! Your order is taken! \n'
-    f'Your total: {price}\n'
-    'Thank you for your order!')
+            price = 0
+            item_ids = []
 
-    send_mail(
-        'Thank You For your Order!',
-        body,
-        'example@example.com',
-        [email],
-        fail_silently=False
-    )
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
 
-    context = {
-        'items': order_items['items'],
-        'price':price
-    }
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
 
-    return render(request, 'customer/order_confirmation.html',context)
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
