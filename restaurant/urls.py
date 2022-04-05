85e698717fe8e2d4b886cd9ee198f2fbffc38e4d
annngure <annwanjikungure@gmail.com>
2022-04-05 10:08:15 +0300

    add models

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
-
 from .views import Dashboard, OrderDetails
-
-
-urlpattern = [
-    path('dashboard/', Dashboard.as_view(), name='dashboard'),
-    path('order/<int:pk>/', OrderDetails.as_view(), name='order-details'),
-
-
-from .views import Dashboard
 
 
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
-
+    path('orders/<int:pk>/', OrderDetails.as_view(), name='order-details'),
 ]
